dc12992232f0288a8ae8b31b2c70dff965731bd3
Daniel Andreas Wang <gamesman36@outlook.com>
2024-01-19 10:16:12 +0100

    Initial commit

---
+++ WordGuesser/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace WordGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testWord = "indefatigable";
+            var shuffler = new StringShuffler(testWord);
+            Console.WriteLine(shuffler.shuffledString);
+        }
+    }
+}
---
+++ WordGuesser/StringShuffler.cs
@@ -0,0 +1,21 @@
+﻿namespace WordGuesser
+{
+    internal class StringShuffler
+    {
+        private readonly Random random = new Random();
+        public string shuffledString { get; private set; }
+
+        public StringShuffler(string input)
+        {
+            ShuffleString(input);
+        }
+        
+        public void ShuffleString(string input)
+        {
+            List<char> characters = new List<char>(input.ToCharArray());
+            characters.Sort((a, b) => random.Next(-1, 2));
+
+            shuffledString = new string(characters.ToArray());
+        }
+    }
+}
